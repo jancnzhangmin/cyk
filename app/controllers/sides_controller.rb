489db0b2fa2641eae0b5c0d6c6474aac2c623f60
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-07-08 07:50:39 +0800

    www

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -1,7 +1,7 @@
 class SidesController < ApplicationController
 
   def index
-    redirect_to 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx685f96097b4c5877&redirect_uri=http://www.cloudtimesoft.net/sides/getcode&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
+    redirect_to 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx685f96097b4c5877&redirect_uri=http://www.cloudtimesoft.com/sides/getcode&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
   end
 
   def getcode
