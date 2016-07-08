7fc173be5defab1fed43559ad59473451c6509b1
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-07-08 11:09:56 +0800

    showpeo

--- app/models/peo.rb
+++ app/models/peo.rb
@@ -1,4 +1,15 @@
 class Peo < ActiveRecord::Base
   belongs_to :stagelevel
   belongs_to :memberlevel
+  has_attached_file :identityfont, :url => "/:attachment/:id/:basename.:extension",  :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+  do_not_validate_attachment_file_type :identityfont
+  has_attached_file :identityback, :url => "/:attachment/:id/:basename.:extension",  :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+  do_not_validate_attachment_file_type :identityback
+  has_attached_file :businessphoto, :url => "/:attachment/:id/:basename.:extension",  :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+  do_not_validate_attachment_file_type :businessphoto
+  has_attached_file :prospectus, :url => "/:attachment/:id/:basename.:extension",  :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+  do_not_validate_attachment_file_type :prospectus
+  has_attached_file :workprove, :url => "/:attachment/:id/:basename.:extension",  :path => ":rails_root/public/:attachment/:id/:basename.:extension"
+  do_not_validate_attachment_file_type :workprove
+
 end
