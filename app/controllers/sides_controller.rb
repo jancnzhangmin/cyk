5e426ad40ed6c1d5730cc2e8e676b8c541971f82
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-07-08 13:13:07 +0800

    json

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -1,12 +1,24 @@
 class SidesController < ApplicationController
 
+  require "open-uri"
+  require 'json'
+
   def index
     redirect_to 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx685f96097b4c5877&redirect_uri=http://www.cloudtimesoft.com/sides/getcode&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
   end
 
   def getcode
     @code = params[:code]
-    redirect_to 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx685f96097b4c5877&secret=89a4867cc23380cc5c6197d809f87ffe&code='+@code+'&grant_type=authorization_code'
+    @response =nil
+    begin
+      open('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx685f96097b4c5877&secret=89a4867cc23380cc5c6197d809f87ffe&code='+@code+'&grant_type=authorization_code') do |http|
+      @response=http.reaad
+      end
+
+    end
+
+
+    #redirect_to 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx685f96097b4c5877&secret=89a4867cc23380cc5c6197d809f87ffe&code='+@code+'&grant_type=authorization_code'
   end
 
 end
