249fa87b68b76225a4f13b5a3a3e7fb4e8f6a82e
jancnzhangmin <jancnzhangmin@hotmail.com>
2016-07-08 08:16:53 +0800

    code

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -5,8 +5,8 @@ class SidesController < ApplicationController
   end
 
   def getcode
-    @code = params['code']
-    #redirect_to 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx685f96097b4c5877&secret=89a4867cc23380cc5c6197d809f87ffe&code='+@code+'&grant_type=authorization_code'
+    @code = params[:code]
+    redirect_to 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx685f96097b4c5877&secret=89a4867cc23380cc5c6197d809f87ffe&code='+@code+'&grant_type=authorization_code'
   end
 
 end
